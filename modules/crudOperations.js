dc0a6639f256de953e85708ffcbdac3252c414c2
Eduardo José de Mello <eduardojose222002@hotmail.com>
2023-06-16 13:43:15 -0300

    Update crudOperations.js

--- modules/crudOperations.js
+++ modules/crudOperations.js
@@ -12,12 +12,7 @@ import { initializeApp } from "firebase/app";
 
 //Firebase Setup
 const firebaseConfig = {
-  apiKey: "AIzaSyA6_x_9i3KK-BTi12qKSV2vSuhPOAkPuwg",
-  authDomain: "projeto-upik.firebaseapp.com",
-  projectId: "projeto-upik",
-  storageBucket: "projeto-upik.appspot.com",
-  messagingSenderId: "1070574617560",
-  appId: "1:1070574617560:web:95722bea975b826e7468ad",
+//Firebase credentials
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
