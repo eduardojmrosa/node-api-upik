fe3626be688f6c7778b31df70ea7d564b56fefa2
Eduardo José de Mello <eduardojose222002@hotmail.com>
2023-06-19 18:43:48 -0300

    Update firebase.js

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,12 +1,7 @@
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyA6_x_9i3KK-BTi12qKSV2vSuhPOAkPuwg",
-  authDomain: "projeto-upik.firebaseapp.com",
-  projectId: "projeto-upik",
-  storageBucket: "projeto-upik.appspot.com",
-  messagingSenderId: "1070574617560",
-  appId: "1:1070574617560:web:95722bea975b826e7468ad",
+ //firestore credentials
 };
 
 const app = initializeApp(firebaseConfig);
